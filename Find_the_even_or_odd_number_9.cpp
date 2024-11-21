62040947525780764425cd8935ab82df6a593686
Hasibul Hassan <115937739+hasibult53@users.noreply.github.com>
2024-11-21 21:14:40 +0600

    Add files via upload

---
+++ Find_the_even_or_odd_number_9.cpp
@@ -0,0 +1,20 @@
+//HH
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout <<"Enter a positive number: ";
+    cin>>a;
+    if(a%2==0){
+        cout<<"In the entered number "<<a<<" is EVEN number.\n";
+    }
+    else
+    {
+
+        cout<<"In the entered number "<<a<<" is ODD number.\n";
+    }
+    return 0;
+}
+
+//HH
---
+++ Print_1_to_100_using_for_loop_7.cpp
@@ -0,0 +1,15 @@
+//HH
+#include <iostream>
+using namespace std;
+int main()
+{
+int input;
+for (input=1; input<=100; input++){
+    cout<<input<<", ";
+}
+
+
+    return 0;
+}
+
+//HH
