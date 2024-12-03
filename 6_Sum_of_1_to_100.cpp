b00305e19369d8fb91f0b6a451ce6f9624b394b7
Hasibul Hassan <115937739+hasibult53@users.noreply.github.com>
2024-12-03 21:31:38 +0600

    Add files via upload

---
+++ 6_Sum_of_1_to_100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int sum=0, i;
+for ( i=1; i<=100; i++){
+        cout<< i <<"\t" ;
+    sum=sum+i;
+
+}
+cout<< "\n The sum of numbers from 1 to 100 is: "<<sum << endl;
+}
+
+// HH
